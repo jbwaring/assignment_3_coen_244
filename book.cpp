b3076c22e8209f3443ebae642f8dd01cd9c71499
jbwaring <jean.baptiste.waring@icloud.com>
2020-02-20 15:13:38 -0500

    cleaned code and added seperation in printing for lisibility

--- book.cpp
+++ book.cpp
@@ -1,25 +1,15 @@
 #include "book.h"
 
-Book::Book(){
-	ISBN = -1;
+Book::Book(){ // Default Constructor
+	
 }
-Book::Book(string at, string tt, long int barcode, int yr, int length, int idd):Reference(at, tt, idd, yr){
-	ISBN = barcode;
+Book::Book(string at, string tt, long int barcode, int yr, int length, int idd):Reference(at, tt, idd, yr){ // Constructor used in main() instantiating an object of parent-class Reference
+	ISBN = barcode; 
 	book_length = length;
 }
 
-int Book::getNumberOfPages(){
+int Book::getNumberOfPages(){ //Returns the number of pages in the book.
 	return book_length;
-}
-
-void Book::setBook(string at, string tt, long int barcode, int yr, int length){
-	ISBN = barcode;
-	book_length = length;
-}
-
-void Book::setRefID(int ref){
-
-	return;
 }
 
 long int Book::getISBN(){
@@ -28,11 +18,11 @@ long int Book::getISBN(){
 
 vector<string> Book::get_ref(){
 	vector<string> ans;
-	ans.push_back(this->get_author().insert(0, "Author: "));
-		ans.push_back(this->get_title().insert(0, "Title: "));
-		ans.push_back(to_string(this->get_year()).insert(0, "Year: "));
+	ans.push_back(get_author().insert(0, "Author: "));
+		ans.push_back(get_title().insert(0, "Title: "));
+		ans.push_back(to_string(get_year()).insert(0, "Year: "));
 		ans.push_back(to_string(ISBN).insert(0, "ISBN: "));
 		ans.push_back(to_string(book_length).insert(0, "Length: "));
-		ans.push_back(to_string(this->get_id()).insert(0, "Reference ID: "));
+		ans.push_back(to_string(get_id()).insert(0, "Reference ID: "));
 	return ans;
 }
--- main.cpp
+++ main.cpp
@@ -7,50 +7,70 @@
 #include "textbook.h"
 #include "ref_man.h"
 using namespace std;
+void println();
 int main() {
 
 Article A1("Joyce", "Araby", 87, 1914, 2, 10, "No info available");
 
 cout << endl << "Number of Pages in Article 1: "<<  A1.getNumberOfPages() << " pages."<<endl;
 
-
+println();
 vector<string> A1_vec = A1.get_ref();
 for(int i =0; i<A1_vec.size(); i++)
 	cout << endl << A1_vec[i];
-cout << endl << endl;
+println();
 Book B1("Thomas Hardy","Far From the Madding Crowd", 983948394333213, 1856, 758, 12);
+Book B2("William Shakespeare", "The Tempest",  9781976805868, 1611, 243, 46);
+
 vector<string> B1_vec = B1.get_ref();
 for(int i =0; i<B1_vec.size(); i++)
 	cout << endl << B1_vec[i];
-cout << endl << endl;
+println();
+
+vector<string> B2_vec = B2.get_ref();
+for(int i =0; i<B2_vec.size(); i++)
+	cout << endl << B2_vec[i];
+println();
+
 TextBook T1("Wiley","Intro to Physics", 983948394333213, 2003, 1054, "wwww.hello.com" ,10);
 vector<string> T1_vec = T1.get_ref();
 for(int i =0; i<T1_vec.size(); i++)
 	cout << endl << T1_vec[i];
-cout << "\n\n\n";
+println();
 
-ReferenceManager R1(5); //Max Number of Ref is 10.
+ReferenceManager R1(6); //Max Number of Ref is 10.
+
 cout << "ReferenceManager:"<< endl;
 R1.add(B1);
 R1.add(T1);
 R1.add(A1);
 R1.add(B1);
 R1.add(T1);
+R1.add(B2);
 
 for(int i = 0; i<R1.get_vec_size(); i++){
 	cout << endl <<  "At Rank " << i << " is Reference with ID: " << R1.get_id(i);
 }
+println();
 
 int search = 87;
 cout << endl << "Is ID " << search << " found? " << R1.ref_search(search)<<endl;
-
+println();
 int del = 2;
-cout << endl << "Reference with ID " << R1.get_id(del) << " is deleted: " << R1.del_ref(del);
+cout << endl << "Reference with ID " << R1.get_id(del) << " is deleted: " << R1.del_ref(del) << endl;
+println();
 cout << endl << "Is ID " << search << " found? " << R1.ref_search(search)<<endl;
+println();
+cout << "ReferenceManager:"<< endl;
 for(int i = 0; i<R1.get_vec_size(); i++){
 	cout << endl <<  "At Rank " << i << " is Reference with ID: " << R1.get_id(i);
 }
+println();
 cout << endl << endl;
 return 0;
 
+}
+
+void println(){
+	cout << endl << "══════════════════════════════════"<<endl;
 }
